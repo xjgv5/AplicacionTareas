e0597550213ab8a7260724eea2c9e7b6dabafa58
Javier Gomez <javier.gov@outlook.es>
2024-01-13 23:48:49 -0600

    capa de servicio y datos

--- src/main/java/com/tareas/servicio/ITareaServicio.java
+++ src/main/java/com/tareas/servicio/ITareaServicio.java
@@ -5,4 +5,10 @@ import java.util.List;
 
 public interface ITareaServicio {
     public List<Tarea> listarTareas();
+
+    public Tarea buscarTareaPorId(Integer idTarea);
+
+    public void  guardarTarea(Tarea tarea);
+
+    public void eliminarTarea(Tarea tarea);
 }
--- src/main/java/com/tareas/servicio/TareaServicio.java
+++ src/main/java/com/tareas/servicio/TareaServicio.java
@@ -1,4 +1,36 @@
 package com.tareas.servicio;
 
+import com.tareas.modelo.Tarea;
+import com.tareas.repositorio.TareaRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class TareaServicio implements ITareaServicio{
+
+    @Autowired
+    private TareaRepositorio tareaRepositorio;
+    @Override
+    public List<Tarea> listarTareas() {
+        return tareaRepositorio.findAll();
+    }
+
+    @Override
+    public Tarea buscarTareaPorId(Integer idTarea) {
+        Tarea tarea = tareaRepositorio.findById(idTarea).orElse(null);
+        return tarea;
+    }
+
+    @Override
+    public void guardarTarea(Tarea tarea) {
+        tareaRepositorio.save(tarea);
+
+    }
+
+    @Override
+    public void eliminarTarea(Tarea tarea) {
+        tareaRepositorio.delete(tarea);
+    }
 }
